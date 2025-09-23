966409e014d61a5b18b82c99d3e812c5eeff65a9
Diego Legarda <dflegarda@gmail.com>
2025-09-23 11:09:12 -0500

    primer commit

---
+++ app/router/users.py
@@ -0,0 +1,81 @@
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+from app.crud.permisos import verify_permissions
+from app.router.dependencies import get_current_user
+from core.database import get_db
+from app.schemas.users import UserCreate, UserOut, UserUpdate
+from app.crud import users as crud_users
+
+router = APIRouter()
+modulo = 1
+
+@router.post("/crear", status_code=status.HTTP_201_CREATED)
+def create_user(
+    user: UserCreate,
+    db: Session = Depends(get_db),
+    user_token: UserOut = Depends(get_current_user)
+):  
+    try:
+        # el rol quien usa el endpoint
+        id_rol = user_token.id_rol
+
+        if (user.id_rol == 1 or user.id_rol == 2):
+            modulo = 2
+        else:
+            modulo = 1
+
+        if not verify_permissions(db, id_rol, modulo, 'insertar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
+
+        crud_users.create_user(db, user)
+        return {"message": "Usuario creado correctamente"}
+    
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/by-email", response_model=UserOut)
+def get_user(
+    email: str, 
+    db: Session = Depends(get_db),
+    user_token: UserOut = Depends(get_current_user)
+):
+    try:
+        # el rol quien usa el endpoint
+        id_rol = user_token.id_rol
+        if not email == user_token.email:
+            if not verify_permissions(db, id_rol, modulo, 'seleccionar'):
+                raise HTTPException(status_code=401, detail="Usuario no autorizado")
+
+        user = crud_users.get_user_by_email(db, email)
+        if not user:
+            raise HTTPException(status_code=404, detail="Usuario no encontrado")
+        return user
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/all-except-admins", response_model=List[UserOut])
+def get_users(
+    db: Session = Depends(get_db),
+    user_token: UserOut = Depends(get_current_user)
+):
+    try:
+        id_rol = user_token.id_rol # el rol del usuario actual
+        if not verify_permissions(db, id_rol, modulo, 'seleccionar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
+        users = crud_users.get_all_user_except_admins(db)
+        return users
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.put("/by-id/{user_id}")
+def update_user(user_id: int, user: UserUpdate, db: Session = Depends(get_db)):
+    try:
+        success = crud_users.update_user_by_id(db, user_id, user)
+        if not success:
+            raise HTTPException(status_code=400, detail="No se pudo actualizar el usuario")
+        return {"message": "Usuario actualizado correctamente"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ db.sql
@@ -0,0 +1,47 @@
+
+CREATE TABLE `roles` (
+	`id_rol` SMALLINT NOT NULL AUTO_INCREMENT UNIQUE,
+	`nombre_rol` VARCHAR(30) NOT NULL,
+	`descripcion` TEXT(500) NOT NULL,
+	PRIMARY KEY(`id_rol`)
+);
+
+INSERT INTO roles (nombre_rol, descripcion) VALUES 
+('superadmin', 'todos los poderes'),
+('administrador', 'todo menos crear superadmin'),
+('supervisor', 'asignar tareas'),
+('operario', 'registrar producción');
+
+DROP TABLE usuarios;
+CREATE TABLE `usuarios` (
+	`id_usuario` INT NOT NULL AUTO_INCREMENT UNIQUE,
+	`nombre` VARCHAR(70) NOT NULL,
+	`id_rol` SMALLINT NOT NULL,
+	`email` VARCHAR(100) NOT NULL UNIQUE,
+	`telefono` CHAR(15) NOT NULL,
+	`documento` VARCHAR(20) NOT NULL,
+	`pass_hash` VARCHAR(140) NOT NULL,
+    `estado` BOOLEAN,
+	PRIMARY KEY(`id_usuario`),
+    FOREIGN KEY(id_rol) REFERENCES roles(id_rol)
+);
+
+
+CREATE TABLE `modulos` (
+	`id_modulo` SMALLINT NOT NULL AUTO_INCREMENT UNIQUE,
+	`nombre_modulo` VARCHAR(30) NOT NULL,
+	PRIMARY KEY(`id_modulo`)
+);
+
+DROP TABLE permisos;
+CREATE TABLE `permisos` (
+	`id_modulo` SMALLINT NOT NULL,
+	`id_rol` SMALLINT NOT NULL,
+	`insertar` BOOLEAN NOT NULL,
+	`actualizar` BOOLEAN NOT NULL,
+	`seleccionar` BOOLEAN NOT NULL,
+	`borrar` BOOLEAN NOT NULL,
+	PRIMARY KEY(`id_modulo`, `id_rol`),
+    FOREIGN KEY(id_rol) REFERENCES roles(id_rol),
+    FOREIGN KEY(id_modulo) REFERENCES modulos(id_modulo)
+);
